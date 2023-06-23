21ecd3d80fea95008c77020db3a1226c8eb26f7e
Adamhanafi22 <adamhanafi1922@gmail.com>
2023-06-23 10:55:37 +0700

    methoud menampilkan

--- CircularQueues_129/CircularQueues_129.cpp
+++ CircularQueues_129/CircularQueues_129.cpp
@@ -8,7 +8,7 @@ class Queues {
 public:
 	Queues() {
 		FRONT = -1;
-		FRONT = -1;
+		REAR = -1;
 
 	}
 
@@ -44,6 +44,21 @@ public:
 			REAR = -1;
 
 		}
+		else {
+			// jika elemen yang dihapus berada di posisi terakhir array,kembali ke awal array
+			if (FRONT == max - 1)
+				FRONT = 0;
+			else
+				FRONT = FRONT + 1;
+		}
+	}
+	void display() {
+		int FRONT_position = FRONT;
+		int REAR_position = REAR;
+
+
+			
+
 
 	}
 }; 
