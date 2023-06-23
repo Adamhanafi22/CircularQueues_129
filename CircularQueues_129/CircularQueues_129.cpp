f2e3326aa340c7de5cf7cecdd9d46d16d5c10575
Adamhanafi22 <adamhanafi1922@gmail.com>
2023-06-23 09:29:26 +0700

    perbandingan

--- CircularQueues_129/CircularQueues_129.cpp
+++ CircularQueues_129/CircularQueues_129.cpp
@@ -18,6 +18,11 @@ public:
 		cin >> num;
 		cout << endl;
 
+		//cek apakah antrian penuh
+		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+			cout << "\nQuenues overflow\n";
+			return;
+		}
 	}
 
 }; 
